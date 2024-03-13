bc7ca642412ef287e6b25c33486c56ef81190387
Veronika Gal <galveron@gmail.com>
2024-03-13 11:36:26 +0100

    feat - create sun data repository interface and class

---
+++ SolarWatchProject/Services/Repositories/ISunDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using SolarWatchProject.Models;
+
+namespace SolarWatchProject.Services.Repositories
+{
+    public interface ISunDataRepository
+    {
+        public IEnumerable<SunRiseAndSetTime> GetAllSunData();
+        public void AddCity(City city);
+        public void AddSunData(SunRiseAndSetTime sunData);
+        City? GetCityByName(string cityName);
+        SunRiseAndSetTime? UpdateSunData(SunRiseAndSetTime sunData);
+        bool DeleteSunData(SunRiseAndSetTime sunData);
+        public SunRiseAndSetTime? GetSunData(string cityName, DateTime date);
+        public SunRiseAndSetTime? GetSunData(int? id);
+        public SunRiseAndSetTime? GetSunDataById(int id);
+    }
+}
---
+++ SolarWatchProject/Services/Repositories/SunDataRepository.cs
@@ -0,0 +1,94 @@
+﻿using SolarWatchProject.Data;
+using SolarWatchProject.Models;
+
+namespace SolarWatchProject.Services.Repositories
+{
+    public class SunDataRepository : ISunDataRepository
+    {
+        private readonly SolarWatchDbContext _dbContext;
+
+        public SunDataRepository(SolarWatchDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<SunRiseAndSetTime> GetAllSunData()
+        {
+            return _dbContext.SunData;
+        }
+        public void AddCity(City city)
+        {
+            _dbContext.Cities.Add(city);
+            _dbContext.SaveChanges();
+        }
+
+        public void AddSunData(SunRiseAndSetTime sunRiseAndSetTime)
+        {
+            _dbContext.SunData.Add(sunRiseAndSetTime);
+            _dbContext.SaveChanges();
+        }
+
+        public City? GetCityByName(string cityName)
+        {
+            if (_dbContext.Cities.Any())
+            {
+                return _dbContext.Cities.FirstOrDefault(
+                    c => c.Name.ToLower() == cityName.ToLower());
+            }
+            return null;
+        }
+
+        public SunRiseAndSetTime? UpdateSunData(SunRiseAndSetTime sunData)
+        {
+            var storedSunData = _dbContext.SunData.FirstOrDefault(
+                c => c.Id == sunData.Id);
+            if (storedSunData != null)
+            {
+                storedSunData.Description = sunData.Description;
+                _dbContext.SaveChanges();
+            }
+            return storedSunData;
+        }
+
+        public bool DeleteSunData(SunRiseAndSetTime sunData)
+        {
+            try
+            {
+                _dbContext.SunData.Remove(_dbContext.SunData.SingleOrDefault(c => c.Id == sunData.Id));
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public SunRiseAndSetTime? GetSunData(string cityName, DateTime date)
+        {
+            if (_dbContext.Cities.Any())
+            {
+                return _dbContext.SunData.FirstOrDefault(
+                s => s.City.ToLower() == cityName.ToLower()
+                     && s.Date == date);
+            }
+            return null;
+        }
+
+        public SunRiseAndSetTime? GetSunData(int? id)
+        {
+            if (_dbContext.Cities.Any())
+            {
+                return _dbContext.SunData.FirstOrDefault(
+                s => s.Id == id);
+            }
+            return null;
+        }
+
+        public SunRiseAndSetTime? GetSunDataById(int id)
+        {
+            return _dbContext.SunData.FirstOrDefault(
+                s => s.Id == id);
+        }
+    }
+}
