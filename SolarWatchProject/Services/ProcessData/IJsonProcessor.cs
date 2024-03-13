ceb76cd0f3862e4acc2503ea03d4aced8a8745f1
Veronika Gal <galveron@gmail.com>
2024-03-13 11:35:33 +0100

    feat - create json processor interface and class

---
+++ SolarWatchProject/Services/ProcessData/IJsonProcessor.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatchProject.Models;
+
+namespace SolarWatchProject.Services.ProcessData
+{
+    public interface IJsonProcessor
+    {
+        City GetProcessedGeoData(string geoData);
+        SunRiseAndSetTime GetProcessedSunData(string sunData, City city, DateTime date);
+    }
+}
---
+++ SolarWatchProject/Services/ProcessData/JsonProcessor.cs
@@ -0,0 +1,54 @@
+﻿using SolarWatchProject.Models;
+using System.Text.Json;
+
+namespace SolarWatchProject.Services.ProcessData
+{
+    public class JsonProcessor : IJsonProcessor
+    {
+        public City GetProcessedGeoData(string geoData)
+        {
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("bármi: " + JsonDocument.Parse(geoData).RootElement.ToString());
+                var result = JsonDocument.Parse(geoData).RootElement[0];
+
+                var cityData = new City()
+                {
+                    Name = result.GetProperty("name").GetString(),
+                    Latitude = result.GetProperty("lat").GetDouble(),
+                    Longitude = result.GetProperty("lon").GetDouble(),
+                    Country = result.GetProperty("country").GetString()
+                };
+
+                return cityData;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error parsing geo data");
+            }
+        }
+
+        public SunRiseAndSetTime GetProcessedSunData(string sunData, City city, DateTime date)
+        {
+            try
+            {
+                var result = JsonDocument.Parse(sunData).RootElement.GetProperty("results");
+
+                var forecast = new SunRiseAndSetTime
+                {
+                    Date = date,
+                    SunRise = $"Sunrise: {result.GetProperty("sunrise").GetString()}",
+                    SunSet = $"Sunset: {result.GetProperty("sunset").GetString()}",
+                    City = city.Name,
+                    Description = ""
+                };
+
+                return forecast;
+            }
+            catch
+            {
+                throw new Exception("Error parsing sun data");
+            }
+        }
+    }
+}
