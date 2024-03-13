eb6c6c3c8a5fab3a3b8eda79a881a8ac5c63d89d
Veronika Gal <galveron@gmail.com>
2024-03-13 11:12:47 +0100

    feat - add dbcontext

---
+++ SolarWatchProject/Data/SolarWatchDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatchProject.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SolarWatchProject.Data
+{
+    public class SolarWatchDbContext : DbContext
+    {
+        public SolarWatchDbContext(DbContextOptions<SolarWatchDbContext> options) : base(options)
+        {
+        }
+        public DbSet<SunRiseAndSetTime> SunRiseAndSetTimes { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = WebApplication.CreateBuilder();
+            var connectionString = builder.Configuration["SolarWatchProgram:ConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<City>()
+                .HasIndex(u => u.Name)
+                .IsUnique()
+                .IsCreatedOnline();
+        }
+    }
+}
