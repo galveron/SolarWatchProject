39477630758fe59ea73bc96a3efd5d37e1b61cb4
Veronika Gal <galveron@gmail.com>
2024-03-13 10:46:01 +0100

    feat - create SunRiseAndSetTime model

---
+++ SolarWatchProject/Models/SunRiseAndSetTime.cs
@@ -0,0 +1,12 @@
+﻿namespace SolarWatchProject.Models
+{
+    public class SunRiseAndSetTime
+    {
+        public int Id { get; init; }
+        public DateTime Date { get; init; }
+        public string City { get; init; }
+        public string SunRise { get; init; }
+        public string SunSet { get; init; }
+        public string? Description { get; set; }
+    }
+}
