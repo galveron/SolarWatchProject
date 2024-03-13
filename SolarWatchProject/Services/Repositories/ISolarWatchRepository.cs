e8424970dd905fa43dc8d5d601ff4a052e09c05c
Veronika Gal <galveron@gmail.com>
2024-03-13 11:36:52 +0100

    feat - create solar watch repository interface and class

---
+++ SolarWatchProject/Services/Repositories/ISolarWatchRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace SolarWatchProject.Services.Repositories
+{
+    public interface ISolarWatchRepository
+    {
+        Task<string> GetSunRiseAndSet(DateTime date, double lat, double lng);
+    }
+}
---
+++ SolarWatchProject/Services/Repositories/SolarWatchRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace SolarWatchProject.Services.Repositories
+{
+    public class SolarWatchRepository : ISolarWatchRepository
+    {
+        public async Task<string> GetSunRiseAndSet(DateTime date, double lat, double lng)
+        {
+            var url = $"https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}&date={date.ToShortTimeString()}";
+            var client = new HttpClient();
+
+            try
+            {
+                var result = await client.GetStringAsync(url);
+                return result;
+            }
+            catch
+            {
+                throw new Exception("Error in SunApi");
+            }
+
+        }
+    }
+}
