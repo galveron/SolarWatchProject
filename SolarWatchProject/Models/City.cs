06e80804b9cb34d578be1ef2eefbc15f56b86ed8
Veronika Gal <galveron@gmail.com>
2024-03-13 10:44:30 +0100

    feat - create City model

---
+++ SolarWatchProject/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarWatchProject.Models
+{
+    public class City
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+        public string Country { get; init; }
+    }
+}
