a889cb110d196f5162e8d27d7b54d4078f2f0404
Veronika Gal <galveron@gmail.com>
2024-03-13 21:54:14 +0100

    change - remove unused properties from User

--- SolarWatchProject/Models/User.cs
+++ SolarWatchProject/Models/User.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SolarWatchProject.Models
 {
+    [Table("users")]
     public class User: IdentityUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
