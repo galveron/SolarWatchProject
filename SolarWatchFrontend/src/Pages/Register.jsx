df22564369b64dc61fc1adca99af2b5fa6d6e3aa
Veronika Gal <galveron@gmail.com>
2024-03-17 17:37:17 +0100

    change UI - modify className in Register

--- SolarWatchFrontend/src/Pages/Register.jsx
+++ SolarWatchFrontend/src/Pages/Register.jsx
@@ -70,7 +70,7 @@ function Register() {
                     </div>
                 </section>
                 :
-                <section className="vh-100 gradient-custom">
+                <section className="main-container gradient-custom">
                     <div className="container py-5 h-100">
                         <div className="row d-flex justify-content-center align-items-center h-100">
                             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
