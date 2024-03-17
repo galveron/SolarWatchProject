be5eba6bba68a2a61c8966d51cbf2952323b769b
Veronika Gal <galveron@gmail.com>
2024-03-17 17:38:02 +0100

    change UI - className in Login

--- SolarWatchFrontend/src/Pages/Login.jsx
+++ SolarWatchFrontend/src/Pages/Login.jsx
@@ -43,7 +43,7 @@ function Login() {
 
     return (
         <>
-            <section className="vh-100 gradient-custom">
+            <section className="main-container  gradient-custom">
                 <div className="container py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-12 col-md-8 col-lg-6 col-xl-5">
