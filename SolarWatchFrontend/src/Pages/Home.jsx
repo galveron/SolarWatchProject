778094696c927137fb1e8fd82021a8c7470b87d5
Veronika Gal <galveron@gmail.com>
2024-03-17 17:41:25 +0100

    change UI - className in Home

--- SolarWatchFrontend/src/Pages/Home.jsx
+++ SolarWatchFrontend/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 const Home = () => (
-    <section className="vh-100 gradient-custom">
+    <section className="main-container gradient-custom">
         <div className="container py-5 h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-12 col-md-8 col-lg-6 col-xl-5 justify-content-center">
