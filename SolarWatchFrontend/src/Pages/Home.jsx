21375be78ada8bc86a7e56308672f951712bd6a0
Veronika Gal <galveron@gmail.com>
2024-03-17 15:40:01 +0100

    feat UI - create Home page

---
+++ SolarWatchFrontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => (
+    <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-12 col-md-8 col-lg-6 col-xl-5 justify-content-center">
+                    <h1 className="fw-bold text-uppercase">Welcome to the Solar Watch app!</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+)
+
+export default Home;
