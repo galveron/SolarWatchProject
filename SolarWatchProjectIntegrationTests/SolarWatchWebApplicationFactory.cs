d9d4b7e1af8cffe3ccab530ccc86b18592a90854
Veronika Gal <galveron@gmail.com>
2024-04-16 12:17:13 +0200

    test - create webApp factory for integration tests

---
+++ SolarWatchProjectIntegrationTests/SolarWatchWebApplicationFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SolarWatchProject.Data;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SolarWatchProjectIntegrationTests
+{
+    public class SolarWatchWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<SolarWatchDbContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                var config = new ConfigurationBuilder()
+                    .AddUserSecrets<SolarWatchWebApplicationFactory>()
+                    .Build();
+
+                services.AddDbContext<SolarWatchDbContext>((container, options) =>
+                {
+                    options.UseSqlServer(config["TestConnectionString"] != null
+                        ? config["TestConnectionString"] : Environment.GetEnvironmentVariable("TESTCONNECTIONSTRING"));
+                });
+
+                var serviceProvider = services.BuildServiceProvider();
+                var scope = serviceProvider.CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<SolarWatchDbContext>();
+                var authSeeder = scope.ServiceProvider.GetRequiredService<AuthenticationSeeder>();
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+                authSeeder.AddRoles();
+                authSeeder.AddAdmin();
+            });
+        }
+    }
+}
